a161dc858ab4b9e919b1cfe53c01d3afefc65161
Doğukan GÖKOVA <info@dogukangokova.me>
2023-02-20 18:12:12 +0300

    Initial Commit

---
+++ DropDownApp/DropDownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownAppApp.swift
+//  DropDownApp
+//
+//  Created by Devinely on 20.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct DropDownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
